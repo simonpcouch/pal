62df561098e4e056c1a6d904fe3b94c461b6b480
simonpcouch <simonpatrickcouch@gmail.com>
2024-10-11 13:31:02 -0500

    solidify "pal directory" idea (#4)

--- R/pal-add-remove.R
+++ R/pal-add-remove.R
@@ -5,7 +5,7 @@
 #' the function a role and prompt, the pal will be available on the command
 #' palette.
 #'
-#' @param role A single string giving the [.pal_init()] role.
+#' @param role A single string giving a descriptor of the pal's functionality.
 # TODO: actually do this once elmer implements
 #' @param prompt A file path to a markdown file giving the system prompt or
 #' the output of [elmer::interpolate()].
@@ -18,36 +18,6 @@
 #' pals" section below.
 #'
 #' @section Adding multiple, persistent pals:
-
-#' Pals can also be added in batch with `.pal_add_dir()`, which takes a directory
-#' of markdown files. Prompts are markdown files with the
-#' name `role-interface.md`, where interface is one of
-#' `r glue::glue_collapse(glue::double_quote(supported_interfaces), ", ", last = " or ")`.
-#' An example directory might look like:
-#'
-#' ```
-#' /
-#' ├── .config/
-#' │   └── pal/
-#' │       ├── proofread-replace.md
-#' │       └── summarize-prefix.md
-#' ```
-#'
-#' In that case, pal will register two custom pals when you call `library(pal)`.
-#' One of them has the role "proofread" and will replace the selected text with
-#' a proofread version (according to the instructions contained in the markdown
-#' file itself). The other has the role "summarize" and will prefix the selected
-#' text with a summarized version (again, according to the markdown file's
-#' instructions). Note:
-#'
-#' * Files without a `.md` extension are ignored.
-#' * Files with a `.md` extension must contain only one hyphen in their filename,
-#'   and the text following the hyphen must be one of `replace`, `prefix`, or
-#'   `suffix`.
-#'
-#' To load custom prompts every time the package is loaded, place your
-#' prompts in `~/.config/pal` (or, to use some other folder, set
-#' `options(.pal_dir = some_dir)` before loading the package).
 #'
 #' @returns
 #' `NULL`, invisibly. Called for its side effect: a pal with role `role`
@@ -124,67 +94,3 @@ parse_interface <- function(interface, role, call = caller_env()) {
 
   paste0(".pal_rs_", role)
 }
-
-# mapping over multiple calls to `.pal_add()` ----------------------------------
-#' @rdname pal_add_remove
-#' @export
-.pal_add_dir <- function(dir) {
-  prompt_paths <- list.files(dir, full.names = TRUE)
-  roles_and_interfaces <- roles_and_interfaces(prompt_paths)
-
-  for (idx in seq_along(prompt_paths)) {
-    role <- roles_and_interfaces[[idx]][1]
-    prompt <- paste0(readLines(prompt_paths[idx]), collapse = "\n")
-    interface <- roles_and_interfaces[[idx]][2]
-
-    .pal_add(role = role, prompt = prompt, interface = interface)
-  }
-}
-
-roles_and_interfaces <- function(prompt_paths) {
-  prompt_basenames <- basename(prompt_paths)
-  prompt_basenames <- grep("\\.md$", prompt_basenames, value = TRUE)
-  prompt_basenames <- filter_single_hyphenated(prompt_basenames)
-
-  roles_and_interfaces <- gsub("\\.md$", "", prompt_basenames)
-  roles_and_interfaces <- strsplit(roles_and_interfaces, "-")
-  roles_and_interfaces <- filter_interfaces(roles_and_interfaces)
-
-  roles_and_interfaces
-}
-
-filter_single_hyphenated <- function(x) {
-  has_one_hyphen <- grepl("^[^-]*-[^-]*$", x)
-  if (any(!has_one_hyphen)) {
-    cli::cli_inform(
-      "Prompt{?s} {.val {paste0(x[!has_one_hyphen], '.md')}} must contain
-       a single hyphen in {?its/their} filename{?s} and will not
-       be registered with pal.",
-      call = NULL
-    )
-  }
-
-  x[has_one_hyphen]
-}
-
-filter_interfaces <- function(x) {
-  interfaces <- lapply(x, `[[`, 2)
-  recognized <- interfaces %in% supported_interfaces
-  if (any(!recognized)) {
-    prompts <- vapply(x, paste0, character(1), collapse = "-")
-    cli::cli_inform(
-      c(
-        "Prompt{?s} {.val {paste0(prompts[!recognized], '.md')}} {?has/have} an
-       unrecognized {.arg interface} noted in {?its/their} filename{?s}
-       and will not be registered with pal.",
-       "{.arg interface} (following the hyphen) must be one of
-        {.or {.code {supported_interfaces}}}."
-      ),
-      call = NULL
-    )
-  }
-
-  x[recognized]
-}
-
-
