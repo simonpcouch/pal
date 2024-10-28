7da14af8c931abd587a9fb6a3a68742b62914e32
simonpcouch <simonpatrickcouch@gmail.com>
2024-10-28 16:20:04 -0500

    document options in a central place

---
+++ R/options.R
@@ -0,0 +1,13 @@
+#' Options used by the pal package
+#'
+#' @description
+#' The pal package makes use of three notable user-facing options:
+#'
+#' * `.pal_dir` is the directory where pal prompts live. See the pal [directory]
+#'   help-page for more information.
+#' * `.pal_fn` and `.pal_args` determine the underlying LLM powering each pal.
+#'   See the "Choosing a model" section of `vignette("pal", package = "pal")`
+#'   for more information.
+#'
+#' @name pal_options
+NULL
